28e82b685b56af629c0bd600254107e2aa11c324
Dávid Smatana <david.smatana@webrock.sk>
2019-01-15 11:29:50 +0100

    Formatting, ignored .history

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      scripts: {
-        src: ['scripts/**/*.js']
-      },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			lib: {
+				src: ['lib/**/*.js']
+			},
+			scripts: {
+				src: ['scripts/**/*.js']
+			},
 			index: {
 				src: ['index.js']
 			}
-    },
+		},
 
 		nodeunit: {
 			all: ['test/*_test.js']
 		}
-  });
+	});
 
-	grunt.registerTask( 'test', ['nodeunit'] );
-  grunt.registerTask( 'default', [
+	grunt.registerTask('test', ['nodeunit']);
+	grunt.registerTask('default', [
 		'jshint'
 	]);
 
